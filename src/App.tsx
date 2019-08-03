61f81e6e4b2a282942b87247b679a435782fcd49
Renal Apriansyah <renal@gmail.com>
2019-08-03 18:39:46 +0700

    implement navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/layout/NavBar';
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       Initialize
     </div>
   );
