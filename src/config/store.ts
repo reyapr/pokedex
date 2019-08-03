f49da4e062267de09e20b534448d5767bb44b3a6
Renal Apriansyah <renal@gmail.com>
2019-08-03 20:16:08 +0700

    refactore store to inject custom middleware

--- src/config/store.ts
+++ src/config/store.ts
@@ -1,14 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import reducer from '../reducer/configReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import logger from 'redux-logger'
+import configureMiddleware from './configureMiddleware'
+
+const middleware = configureMiddleware()
 
 const storeEnhancer = composeWithDevTools(
-  applyMiddleware(
-    thunk,
-    ...(process.env.NODE_ENV === 'development' ?  [logger]: [] )
-  )
+  applyMiddleware(...middleware)
 )
 
 const store = createStore(reducer, storeEnhancer)
