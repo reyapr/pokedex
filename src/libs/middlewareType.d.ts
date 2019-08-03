15093ecd153c5bd69ad9654a30c66227577c1b26
Renal Apriansyah <renal@gmail.com>
2019-08-03 20:00:31 +0700

    create type module for custom middleware

---
+++ src/libs/middlewareType.d.ts
@@ -0,0 +1,19 @@
+declare module "configMiddlewareType" {
+  import { Method, AxiosResponse } from "axios"
+
+  export interface Init {
+    params?: object
+    method?: Method
+    headers?: object
+    body?: any
+  }
+
+  export type GetState = () => State;
+  export type DepsFetch = (string, Init?) => Promise<AxiosResponse>
+
+  export interface Middleware {
+    fetch: DepsFetch
+    getState: GetState
+    dispatch: Dispatch
+  }
+}
