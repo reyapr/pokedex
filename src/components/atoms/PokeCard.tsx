d1b822e8a75959380a7ed23fb46f04cb8f4cf27a
Renal Apriansyah <renal@gmail.com>
2019-08-03 22:03:47 +0700

    render data by props

--- src/components/atoms/PokeCard.tsx
+++ src/components/atoms/PokeCard.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react'
 
-export interface Props {} 
+export interface Props {
+  pokemonId?: any,
+  name: string
+} 
 
-const PokeCard: React.FC = () => {
-
+const PokeCard: React.FC<Props> = ({ name, pokemonId }) => {
+  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`
   return (
-    <div className="col-md-3 card pt-3 mx-0-auto" style={{margin: "40px"}} >
-      <img src="http://bit.ly/nancy3" className="card-img-top" alt="pokemon"/>
+    <div className="col-md-3 card mx-0-auto " style={{margin: "40px", padding: '0px'}} >
+      <img src={imageUrl} className="card-img-top bg-info" alt="pokemon"/>
       <div className="card-body">
-        <p className="card-text">TEXT</p>
+        <p className="card-text text-center">{name}</p>
       </div>
     </div>
   )
