e7b5fcef3ce2066b8e1c6d55eeb65da906199a85
Renal Apriansyah <renal@gmail.com>
2019-08-03 18:36:34 +0700

    initiliaze with navbar

---
+++ src/components/layout/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const layout: React.FC = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark fixed-top">
+      <a className="navbar-brand text-light">Pokedex</a>
+    </nav>
+  )
+}
+
+export default layout
