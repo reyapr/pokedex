cef17bfc660e88f4e4b8cb2f05dbd868c40e13e2
Renal Apriansyah <renal@gmail.com>
2019-08-04 21:00:11 +0700

    add sort by number

--- src/libs/sortHelper.ts
+++ src/libs/sortHelper.ts
@@ -10,3 +10,17 @@ export const sortArr = (data: Array<any>, sortBy: string) => {
     return 0
   })
 }
+
+export const sortByNumber = (data: Array<any>, sortBy: string) => {
+  return data.sort((a,b)=>{
+    let splittedB = b.url.split('/')
+    let splittedA = a.url.split('/')
+    let idB = Number(splittedB[splittedB.length-2])
+    let idA = Number(splittedA[splittedA.length-2])
+    if(sortBy=='asc'){
+      return idA-idB
+    }else{
+      return idB-idA
+    }
+  })
+}
