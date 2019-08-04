4a4ed1d1c5356d665b119e3f93fa569342e97c5b
Renal Apriansyah <renal@gmail.com>
2019-08-04 20:59:21 +0700

    implement route

--- src/components/atoms/PokeCard.tsx
+++ src/components/atoms/PokeCard.tsx
@@ -1,18 +1,19 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export interface Props {
-  pokemonDigitId?: string,
+  pokemonDigitId: string,
   name: string
 } 
 
 const PokeCard: React.FC<Props> = ({ name, pokemonDigitId }) => {
   const imageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemonDigitId}.png`
   return (
-    <div className="col-md-3 card mx-0-auto " style={{margin: "40px", padding: '0px'}} >
+    <div className="col-md-3 card mx-0-auto " style={{margin: "40px", padding: '0px'}}>
       <img src={imageUrl} className="card-img-top bg-info" alt="pokemon"/>
-      <div className="card-body">
+      <div className="card-body text-center">
         <p className="card-text text-center">#{pokemonDigitId}</p>
-        <p className="card-text text-center">{name}</p>
+        <Link className="card-text text-center" to={`/pokemon/${pokemonDigitId.slice(-1)}`}>{name}</Link>
       </div>
     </div>
   )
